7c7fbaac95f6375f685961550a43430a84636d72
Іван <gendalf2003@gmail.com>
2021-03-21 16:28:44 +0200

    lab3.2

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Point.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Point A;
+			A.SetX(5);
+			int test = A.GetX();
+			Assert::AreEqual(test, 2);
+		}
+	};
+}
